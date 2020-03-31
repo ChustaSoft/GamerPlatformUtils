2e83075525dd0aa78a60edce7eb7f507f9b7fb39
Xelit3 <55638917+Xelit3@users.noreply.github.com>
2020-03-31 21:45:09 +0200

    #1: Project structure created (#14)
    * #1: Project structure created
    
    * PR changes: Async naming convention word missing on async methods

---
+++ ChustaSoft.GamersPlatformUtils.Abstractions/Contracts/IPlatformFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChustaSoft.GamersPlatformUtils.Abstractions.Contracts
+{
+    public interface IPlatformFactory
+    {
+
+        Task<IEnumerable<IPlatform>> GetPlatformsAsync();
+
+        Task<IEnumerable<IAnalyzer>> GetAnalyzersAsync();
+
+        Task<IEnumerable<ICleaner>> GetCleanersAsync();
+
+    }
+}
---
+++ GamersPlatformUtils/Implementations/PlatformFactory.cs
@@ -0,0 +1,25 @@
+﻿using ChustaSoft.GamersPlatformUtils.Abstractions.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ChustaSoft.GamersPlatformUtils.Domain.Implementations
+{
+    public class PlatformFactory : IPlatformFactory
+    {
+        public Task<IEnumerable<IAnalyzer>> GetAnalyzersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ICleaner>> GetCleanersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IPlatform>> GetPlatformsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
