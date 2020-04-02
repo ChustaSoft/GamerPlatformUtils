31ad4c48cc53d69113ff8916d5f97f10b24fd062
Victor <diblix69@hotmail.com>
2020-04-02 20:37:44 +0200

    - Definition of interfaces - Creation of ILinkAssigner and ILinkFinder - Creation of models

--- ChustaSoft.GamersPlatformUtils.Abstractions/Contracts/ILinkFinder.cs
+++ ChustaSoft.GamersPlatformUtils.Abstractions/Contracts/ILinkFinder.cs
@@ -1,12 +1,15 @@
 ﻿using ChustaSoft.GamersPlatformUtils.Abstractions.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace ChustaSoft.GamersPlatformUtils.Abstractions.Contracts
 {
-    public interface ICleaner
+    public interface ILinkFinder
     {
-        Task<CleanResult> CleanAsync(IEnumerable<FileInfo> paths);
+        string RootFolderName { get; set; }
+
+        Task<IEnumerable<GameLink>> Find();
     }
 }
---
+++ ChustaSoft.GamersPlatformUtils.Abstractions/Models/Platform.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChustaSoft.GamersPlatformUtils.Abstractions.Models
+{
+    public class Platform
+    {
+    }
+}
