856aa7198e1c333c8ccd15eb610170dbf4bcf263
Blanco Jairo <jairo.blanco@cotecna.es>
2020-04-27 10:57:47 +0200

    Implement Cleaner for Steam

--- ChustaSoft.GamersPlatformUtils.Abstractions/Models/CleanResult.cs
+++ ChustaSoft.GamersPlatformUtils.Abstractions/Models/CleanResult.cs
@@ -1,8 +1,18 @@
-﻿namespace ChustaSoft.GamersPlatformUtils.Abstractions
+﻿using System.Collections.Generic;
+
+namespace ChustaSoft.GamersPlatformUtils.Abstractions
 {
     public class CleanResult
     {
         public bool Success { get; set; }
         public int CleanedDirectories { get; set; }
+        public IDictionary<string, string> ErrorList { get; set; }
+
+        public CleanResult()
+        {
+            ErrorList = new Dictionary<string, string>();
+            Success = true;
+            CleanedDirectories = 0;
+        }
     }
 }
