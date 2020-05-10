a42c89e35d4ed4b6ea4683b2a9e0084afd45a508
Xavi Abelaira Rueda <xavi_abelaira@outlook.com>
2020-05-11 01:00:17 +0200

    FullName show after analysis performed

---
+++ ChustaSoft.GamersPlatformUtils.UI/Helpers/FileInfoConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ChustaSoft.GamersPlatformUtils.UI.Helpers
+{
+    public class FileInfoConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
